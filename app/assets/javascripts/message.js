1d1803668be7ffd138122eac7ee257029f7e595d
yosihik <uyu0802525da@gmail.com>
2020-05-05 21:28:25 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -65,11 +65,13 @@ $('#new_message').on('submit', function(e){
 
 var reloadMessages = function() {
   var last_message_id = $('.messages:last').data("message-id");
-  console.log(last_message_id)
   $.ajax({
+    //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
     url: "api/messages",
+    //ルーティングで設定した通りhttpメソッドをgetに指定
     type: 'get',
     dataType: 'json',
+    //dataオプションでリクエストに値を含める
     data: {id: last_message_id}
   })
   .done(function(messages) {

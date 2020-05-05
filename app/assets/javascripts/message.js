0921a5805ab3c39d355b8adda772c63ded782f4d
yosihik <uyu0802525da@gmail.com>
2020-05-05 21:13:26 +0900

    comitto

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
    if ( message.image ) {
      var html =
-      `<div class="messages">
+      `<div class="messages" data-message-id="${message.id}">
          <div class="messages__box">
            <div class="messages__box__name">
              ${message.user_name}
@@ -16,12 +16,12 @@ $(function(){
              ${message.content}
            </p>
          </div>
-         <img src=${message.image} class = "lower-message__image" >
+         <img src=${message.image} >
        </div>`
      return html;
    } else {
      var html =
-      `<div class="messages">
+      `<div class="messages" data-message-id="${message.id}">
          <div class="messages__box">
            <div class="messages__box__name">
              ${message.user_name}
@@ -62,4 +62,35 @@ $('#new_message').on('submit', function(e){
       alert("メッセージ送信に失敗しました");
   });
 })
+
+var reloadMessages = function() {
+  //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+  var last_message_id = $('.messages:last').data("message-id");
+  console.log(last_message_id)
+  $.ajax({
+    //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+    url: "api/messages",
+    //ルーティングで設定した通りhttpメソッドをgetに指定
+    type: 'get',
+    dataType: 'json',
+    //dataオプションでリクエストに値を含める
+    data: {id: last_message_id}
+  })
+  .done(function(messages) {
+    if (messages.length !== 0) {
+    var insertHTML = '';
+    $.each(messages, function(i, message) {
+      insertHTML += buildHTML(message)
+    });
+    $('.main-message').append(insertHTML);
+    $('.main-message').animate({ scrollTop: $('.main-message')[0].scrollHeight});
+  }
+  })
+  .fail(function() {
+    alert('error');
+  });
+};
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });

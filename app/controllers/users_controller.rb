89a0b7f24351601c89f73176739148f8d8135e78
yosihik <uyu0802525da@gmail.com>
2020-05-05 12:25:57 +0900

    comit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
 
+  def index
+    return nil if params[:keyword] == ""
+    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
+    respond_to do |format|
+     format.html
+     format.json
+    end
+  end
+
   def edit
   end
 
@@ -10,6 +19,7 @@ class UsersController < ApplicationController
       render :edit
     end
   end
+
 
   private
 

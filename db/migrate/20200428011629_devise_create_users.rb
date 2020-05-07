c0a5a50e8d7784d76b0bd9d4f794aab246384472
yosihik <uyu0802525da@gmail.com>
2020-05-07 15:12:06 +0900

    Update 20200428011629_devise_create_users.rb

--- db/migrate/20200428011629_devise_create_users.rb
+++ db/migrate/20200428011629_devise_create_users.rb
@@ -2,7 +2,7 @@
 
 class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
-    create_table :users do |t|
+    change_table :users do |t|
       ## Database authenticatable
       t.string :name,               null: false
       t.string :email,              null: false, default: ""
